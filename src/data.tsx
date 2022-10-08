f8926af92087fe152434ba3e715dbbd816c6d091
JJJ4032002 <rp4888888@gmail.com>
2022-10-08 21:40:08 +0530

    Add data

---
+++ src/data.tsx
@@ -0,0 +1,35 @@
+import HomeIcon from "@mui/icons-material/Home";
+import BookIcon from "@mui/icons-material/Book";
+import LogoutIcon from "@mui/icons-material/Logout";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+
+const HomeIconWrapper = () => (
+  <ListItemIcon>
+    <HomeIcon />
+  </ListItemIcon>
+);
+const BookmarkIconWrapper = () => (
+  <ListItemIcon>
+    <BookIcon />
+  </ListItemIcon>
+);
+const SignOutIconWrapper = () => (
+  <ListItemIcon>
+    <LogoutIcon />
+  </ListItemIcon>
+);
+
+const BrightnessIconWrapper = () => (
+  <ListItemIcon>
+    <Brightness7Icon />
+  </ListItemIcon>
+);
+const MenuData = [
+  { Title: "Home", Icon: HomeIconWrapper },
+  { Title: "Bookmarked", Icon: BookmarkIconWrapper },
+  { Title: "Light or Dark", Icon: BrightnessIconWrapper },
+  { Title: "Sign Out", Icon: SignOutIconWrapper },
+];
+
+export { MenuData };
