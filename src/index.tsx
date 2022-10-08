6a0ed75aa3b635fa89b0b130dc965d91f7a9fc2d
JJJ4032002 <rp4888888@gmail.com>
2022-10-08 21:44:51 +0530

    Changes - 1. Add Theme Context

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { Theme } from "./Contexts/Theme";
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Theme>
+        <App />
+      </Theme>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
