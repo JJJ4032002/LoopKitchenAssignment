4f51da46ce98fe9d6f45f8d45e045a79cd20a672
JJJ4032002 <rp4888888@gmail.com>
2022-10-08 21:40:44 +0530

    Add SideBar

---
+++ src/Components/Home/Components/SideBar.tsx
@@ -0,0 +1,82 @@
+import React, { useContext } from "react";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Box from "@mui/material/Box";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import { Divider, ListItemIcon } from "@mui/material";
+import { MenuData } from "../../../data";
+import { ThemeContext } from "../../../Contexts/Theme";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import styled from "styled-components/macro";
+const Heading = styled.h1`
+  color: ${(props) => props.theme.palette.primary.main};
+  padding: 0.5em 0.5em;
+`;
+function SideBar({
+  open,
+  handleSidebar,
+}: {
+  open: boolean;
+  handleSidebar: (state: boolean) => void;
+}) {
+  let { mode, toggleColorMode } = useContext(ThemeContext);
+  return (
+    <Drawer
+      open={open}
+      anchor="left"
+      onClose={() => {
+        handleSidebar(false);
+      }}
+      ModalProps={{ keepMounted: true }}
+    >
+      <Box role="presentation" sx={{ width: 250 }}>
+        <List>
+          <Heading>Loop Kitchen</Heading>
+        </List>
+        <Divider></Divider>
+        <List>
+          {MenuData.map((element) => {
+            return (
+              <ListItem
+                onClick={
+                  element.Title === "Light or Dark"
+                    ? toggleColorMode
+                    : undefined
+                }
+                key={element.Title}
+                disablePadding
+              >
+                <ListItemButton>
+                  {element.Title == "Light or Dark" ? (
+                    <>
+                      {mode === "light" ? (
+                        element.Icon()
+                      ) : (
+                        <ListItemIcon>
+                          <Brightness4Icon />
+                        </ListItemIcon>
+                      )}
+
+                      <ListItemText
+                        primary={mode === "light" ? "Light mode" : "Dark Mode"}
+                      ></ListItemText>
+                    </>
+                  ) : (
+                    <>
+                      {element.Icon()}{" "}
+                      <ListItemText primary={element.Title}></ListItemText>
+                    </>
+                  )}
+                </ListItemButton>
+              </ListItem>
+            );
+          })}
+        </List>
+      </Box>
+    </Drawer>
+  );
+}
+
+export default SideBar;
