8444183d283772822f26cf16e2c8bd00c69d5bf3
JJJ4032002 <rp4888888@gmail.com>
2022-10-09 00:47:16 +0530

    Changes - 1. Completed Autocomplete Input. 2. Ability to add Restaurants

--- src/Components/Restaurants/Restaurants.tsx
+++ src/Components/Restaurants/Restaurants.tsx
@@ -1,10 +1,11 @@
 import { Button, IconButton, TextField } from "@mui/material";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import RestaurantPlaceholder from "../../assets/Images/RestaurantPlaceholder.svg";
 import MenuIcon from "@mui/icons-material/Menu";
 import useWindowSize from "../../hooks/useWindowSize";
 import Autocomplete from "@mui/material/Autocomplete";
 import { RestaurantsDataContext } from "../../Contexts/RestaurantsData";
+import { v4 as uuidv4 } from "uuid";
 import {
   Heading,
   Header,
@@ -13,11 +14,41 @@ import {
   Wrapper,
   Placeholder,
   Image,
+  Frame,
+  Block,
+  BlockHeader,
 } from "./RestaurantsCss";
+import { NewRestaurantsDataType } from "../../helpers/GetRestaurantsData";
 function Restaurants({ handleOpen }: { handleOpen: (state: boolean) => void }) {
-  let { Restaurants } = useContext(RestaurantsDataContext);
-  console.log(Restaurants);
+  let { Restaurants, AddedRestaurants, AddToRestaurants } = useContext(
+    RestaurantsDataContext
+  );
+  let [InputValue, setInputValue] = useState("");
+  let [value, setValue] = useState<NewRestaurantsDataType | null>(null);
   let disableDrawer = useWindowSize();
+  function handleChangeInput(event: any, newValue: string) {
+    setInputValue(newValue);
+  }
+  function handleValueChange(
+    event: any,
+    NewValue: NewRestaurantsDataType | null
+  ) {
+    setValue(NewValue);
+  }
+  function handleSubmit() {
+    let id = uuidv4();
+    if (value !== null) {
+      let newRestaurant = {
+        Id: id,
+        Name: value.label,
+        Bookmarked: false,
+      };
+      AddToRestaurants(newRestaurant);
+      setValue(null);
+      setInputValue("");
+    }
+  }
+  let disableButton = value !== null ? (value.label ? false : true) : true;
   return (
     <Wrapper>
       <Header>
@@ -40,6 +71,10 @@ function Restaurants({ handleOpen }: { handleOpen: (state: boolean) => void }) {
           <Autocomplete
             disablePortal
             options={Restaurants}
+            value={value}
+            onChange={handleValueChange}
+            inputValue={InputValue}
+            onInputChange={handleChangeInput}
             renderInput={(params) => (
               <TextField
                 {...params}
@@ -54,13 +89,33 @@ function Restaurants({ handleOpen }: { handleOpen: (state: boolean) => void }) {
           "Reload Again"
         )}
 
-        <Button variant="contained">Add</Button>
+        <Button
+          onClick={handleSubmit}
+          disabled={disableButton}
+          variant="contained"
+        >
+          Add
+        </Button>
       </SearchBarWrapper>
       <RestaurantWrapper>
-        <Placeholder>
-          <Image src={RestaurantPlaceholder} alt="" />
-          <Heading>No Restaurants added yet!</Heading>
-        </Placeholder>
+        {AddedRestaurants === null ? (
+          <Placeholder>
+            <Image src={RestaurantPlaceholder} alt="" />
+            <Heading>No Restaurants added yet!</Heading>
+          </Placeholder>
+        ) : (
+          <>
+            {AddedRestaurants.map((element) => {
+              return (
+                <Block key={element.Id}>
+                  <BlockHeader>
+                    <h2>{element.Name}</h2>
+                  </BlockHeader>
+                </Block>
+              );
+            })}
+          </>
+        )}
       </RestaurantWrapper>
     </Wrapper>
   );
