9c2d5a47d3fb8d159b0c0195b92cacd1fbddcf91
JJJ4032002 <rp4888888@gmail.com>
2022-10-08 21:41:57 +0530

    Add GetRestaurantsData

---
+++ src/helpers/GetRestaurantsData.ts
@@ -0,0 +1,27 @@
+interface RestaurantDataType {
+  id: string;
+  createdTime: string;
+  fields: {
+    Name: string;
+  };
+}
+interface NewRestaurantsDataType {
+  Name: string;
+}
+async function GetRestaurantsData(): Promise<NewRestaurantsDataType[]> {
+  let result = await fetch(
+    "https://api.airtable.com/v0/appjWdL7YgpxIxCKA/restaurants?view=Grid%20view",
+    {
+      headers: {
+        Authorization: "Bearer keyfXgn8PL6pB3x32",
+      },
+    }
+  );
+  let resultFromJson = await result.json();
+  let newData = resultFromJson.records.map((element: RestaurantDataType) => {
+    return { label: element.fields.Name };
+  });
+  return newData;
+}
+export default GetRestaurantsData;
+export type { NewRestaurantsDataType };
