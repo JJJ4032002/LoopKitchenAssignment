eb6addf50737b79e983f0ad606bead2bd5349fe0
JJJ4032002 <rp4888888@gmail.com>
2022-10-09 14:52:19 +0530

    Add Map

---
+++ src/Components/Elements/Map.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components/macro";
+import { Button } from "@mui/material";
+interface addedRestaurantsDataType {
+  Id: string;
+  Name: string;
+  Bookmarked: boolean;
+}
+const Frame = styled.iframe`
+  width: 100%;
+  height: 100%;
+  border: none;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+`;
+const Block = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+`;
+const BlockHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+function Map({
+  element,
+  handleBookmark,
+}: {
+  element: addedRestaurantsDataType;
+  handleBookmark: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}) {
+  return (
+    <Block key={element.Id}>
+      <Frame
+        src={`https://datastudio.google.com/embed/u/0/reporting/430242fa-4162-4950-a984-824b3b355b3c/page/dQMwC?params=%7B%22ds2.name2%22:%22${element.Name}%22%7D`}
+      ></Frame>
+      <BlockHeader>
+        <h2>{element.Name}</h2>
+        <Button
+          onClick={handleBookmark}
+          data-unique={`${element.Id}`}
+          variant="contained"
+        >
+          {element.Bookmarked ? "UnBookmark" : "Bookmark"}
+        </Button>
+      </BlockHeader>
+    </Block>
+  );
+}
+
+export default Map;
