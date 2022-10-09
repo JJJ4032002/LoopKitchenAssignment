ef9f391339c3db0259a363cf618289ed8da32ab8
JJJ4032002 <rp4888888@gmail.com>
2022-10-09 14:54:04 +0530

    Add css

--- src/Components/Restaurants/RestaurantsCss.tsx
+++ src/Components/Restaurants/RestaurantsCss.tsx
@@ -39,18 +39,35 @@ const RestaurantWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  padding: 1em;
 `;
 const Heading = styled.h1`
   color: ${(props) => props.theme.palette.primary.main};
 `;
 
 const Frame = styled.iframe`
-  width: 50%;
+  width: 100%;
+  height: 100%;
+  border: none;
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
 `;
-const Block = styled.div``;
+const Block = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5em;
+`;
 const BlockHeader = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
+`;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(min(15rem, 100%), 1fr));
+  grid-auto-rows: 18rem;
+  width: 100%;
+  grid-gap: 3em 3em;
 `;
 export {
   Heading,
@@ -63,4 +80,5 @@ export {
   Frame,
   Block,
   BlockHeader,
+  Grid,
 };
